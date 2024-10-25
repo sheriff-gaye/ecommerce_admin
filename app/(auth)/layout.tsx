c19fd1c221e2f250909f26d9e6c33fce863d639d
sheriff-gaye <sheriffgaye5@gmail.com>
2024-10-25 01:44:04 +0000

    all

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,6 +4,6 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className=" h-full flex items-center justify-center">{children}</div>
+    <div className=" h-screen  flex items-center justify-center">{children}</div>
   );
 }
